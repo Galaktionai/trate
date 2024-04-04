980181c479c6717cb60470c5ad7cc69edf0713b8
Galaktionai <galaktion.tatarinow@yandex.ru>
2024-04-05 06:30:51 +0900

    four

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,32 @@ $(document).ready(function() {
     $("body").toggleClass("dont-scroll"); 
   })
 
+  let header = document.querySelector('.header__bottom');
 
-  $('.header__adaptiv__menu__nav_li, .header__adaptiv__menu__nav__2_menu').click(function() {
-    $('.header__adaptiv__menu__nav__2_menu').toggleClass('active');
+  window.addEventListener('scroll', function () {
+    let scrollDistance = window.scrollY;
+    if (scrollDistance >= 39) {
+      header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
   })
 
-  $('.header__adaptiv__menu__nav__2_menu__back').click(function() {
-    $('.header__adaptiv__menu__nav__2_menu').toggleClass('active');
-  })
+  // $('.header__adaptiv__menu__nav_li, .header__adaptiv__menu__nav__2_menu').click(function() {
+  //   $('.header__adaptiv__menu__nav__2_menu').toggleClass('active');
+  // })
 
-  $('.order-call, .modal-order-call__bacgroaund, .call__wrap__exit').click(function() {
+  // $('.header__adaptiv__menu__nav__2_menu__back').click(function() {
+  //   $('.header__adaptiv__menu__nav__2_menu').toggleClass('active');
+  // })
+
+  $('.order-call, .call__wrap__exit').click(function() {
     $('.modal-order-call').toggleClass('active');
+    $("body").toggleClass("dont-scroll"); 
+  })
+
+  $('.brix---accordion-items, .modal_detail__exit').click(function() {
+    $('.modal_detail_info').toggleClass('active');
     $("body").toggleClass("dont-scroll"); 
   })
 
@@ -122,7 +137,7 @@ $(document).ready(function() {
   });
 
 
-  $('.keys__filter__form__select, .feedback__green__form__select').niceSelect();
+  $('.keys__filter__form__select, .feedback__green__form__select, .contacts__grid__form__select').niceSelect();
 
   $("#filter-btn-news").click(function() {
     $(".news__filter__btns").toggleClass("active");      
@@ -133,4 +148,73 @@ $(document).ready(function() {
     $(".news__filter__btns").toggleClass("active");       
     $("body").removeClass("dont-scroll"); 
   })
+
+  // let eventsFixed = document.querySelector('.frame-1244');
+
+  // window.addEventListener('scroll', function () {
+  //   let scrollDistance = window.scrollY;
+  //   if (scrollDistance >= 1250) {
+  //     eventsFixed.classList.add("fixed");
+  //   } else {
+  //     eventsFixed.classList.remove("fixed");
+  //   }
+  // })
+
+  // $(".implant_details_info__grid__txt__li").click(function() {
+  //   $(this).toggleClass("active");
+
+  //   let active = document.querySelectorAll('.implant_details_info__grid__txt__li');
+  //   if(this.classList.contains('active')){
+  //     active.removeClass('active');
+  //     console.log('ewgwg')
+  //   }
+  // })
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var liElements = document.querySelectorAll('.implant_details_info__grid__txt__li');
+  var imgElements = document.querySelectorAll('.implant_details_info__grid__img__replace');
+
+  liElements.forEach(function(li, index) {
+      li.addEventListener('click', function() {
+          // Удаление класса active у всех элементов списка
+          liElements.forEach(function(li) {
+            li.classList.remove('active');
+          });
+          // Добавление класса active к текущему элементу списка
+          this.classList.add('active');
+
+          // Удаление класса active у всех изображений
+          imgElements.forEach(function(img) {
+              img.classList.remove('active');
+          });
+          // Добавление класса active к соответствующему изображению
+          imgElements[index].classList.add('active');
+      });
+  });
+
+  var headerA = document.getElementsByClassName("header__adaptiv__menu__nav_a");
+  var int;
+
+  for (var int = 0; int < headerA.length; int++) {
+    headerA[int].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel) {
+            panel.classList.toggle("active");
+            console.log('click');
+        }
+    });
+    // Добавляем обработчик для кнопки backBtn
+    let backBtn = headerA[int].nextElementSibling.querySelector('.header__adaptiv__menu__nav__2_menu__back');
+
+    if(backBtn){
+        backBtn.addEventListener("click", function() {
+            var panel = this.parentElement;
+            panel.classList.toggle("active");
+        });
+    }
+  }
+});
+
+
